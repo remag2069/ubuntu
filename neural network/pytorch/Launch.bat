8ba42a250ccf78c74f979a7c711b0752c234591e
remag2069 <remag2069@gmail.com>
2020-03-03 07:38:25 +0530

    m

--- neural network/pytorch/Launch.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo  welcome wait till boot up ...
-D:
-cd D:\python projects\neural network\pytorch
-jupyter lab
--- searcheingine/background.py
+++
@@ -1,7 +0,0 @@
-from tkinter import *
-window=Tk()
-pic=PhotoImage(file='D:\\python projects\\searcheingine\\d.gif')
-c=Canvas(width=pic.width,height=pic.height)
-c.create_image(0,0,image=pic,anchor=NW)
-c.pack()
-window.mainloop()
